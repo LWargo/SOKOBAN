b48451d369741cb778f4f52ff8beba6ef09d2290
Britton Horn <bhorn@trinity.edu>
2024-09-04 13:32:04 -0500

    Updated with 9/4 code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jumpPower;
     private List<KeyCode> keysPressed = new List<KeyCode>();
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private float horizontal;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +31,8 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.D)) {
             //keysPressed.Remove(KeyCode.D);
         }
+        if (isFacingRight && horizontal < 0f) Flip();
+        if (!isFacingRight && horizontal > 0f) Flip();
     }
 
     void FixedUpdate() {
@@ -36,9 +43,30 @@ public class PlayerMovement : MonoBehaviour
         else {
             rb.velocity = new Vector2(0,rb.velocity.y);
         }*/
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    void OnJump() {
+    void OnJump(InputValue value) {
         Debug.Log("Jumping!");
+        if (value.isPressed && IsGrounded())
+            rb.velocity = new Vector2(rb.velocity.x,jumpPower);
+        else if(rb.velocity.y > 0f) rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y * .5f);
+    }
+
+    void OnMove(InputValue value) {
+        Debug.Log("Moving: " + value.Get<float>());
+        horizontal = value.Get<float>();
+    }
+
+    private bool IsGrounded() {
+        return Physics2D.OverlapCircle( groundCheck.position, .1f, groundLayer);
+    }
+
+    private void Flip() {
+        isFacingRight = !isFacingRight;
+        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
